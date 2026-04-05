d4cb4903ad27ba3be2e262e9f5821eeaee5fc74e
jellyma34 <152998511+jellyma34@users.noreply.github.com>
2026-04-05 14:51:24 +0800

    fix: open cors for debugging

--- backend/app/main.py
+++ backend/app/main.py
@@ -50,11 +50,7 @@ async def preflight_handler():
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://gordo-frontend-production.up.railway.app",
-        "http://localhost:3000",
-        "http://127.0.0.1:3000",
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
